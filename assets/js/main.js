e4d6147b880e0300af5f2cc62c940cd5254ab3e1
Jacob Mortensen <jacob.w.mortensen@gmail.com>
2015-05-08 09:53:59 -0600

    Added temperature selector and headings for selectors

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,19 @@ L.mapbox.accessToken = 'pk.eyJ1Ijoiandtb3J0ZW5zZW4iLCJhIjoiQjJHSVp4NCJ9.AYH98hv0
 var map = L.mapbox.map('map').setView([29.785, -95.39], 10);
 L.mapbox.tileLayer('mapbox.outdoors').setZIndex(1).addTo(map);
 var layers = {
-  "000": L.mapbox.tileLayer('jwmortensen.z2iq9f6r'),
-  "100": L.mapbox.tileLayer('jwmortensen.juelv7vi')
+  "0000": L.mapbox.tileLayer('jwmortensen.z2iq9f6r'),
+  "1000": L.mapbox.tileLayer('jwmortensen.juelv7vi')
   }
-addLayer("000");
+addLayer("0000");
 
 var race = document.getElementById('race');
 var gender = document.getElementById('gender');
 var age = document.getElementById('age');
+var temp = document.getElementById('temp');
 race.onchange = updateLayer
 gender.onchange = updateLayer
 age.onchange = updateLayer
+temp.onchange = updateLayer
 
 function updateLayer(e) {
   e.preventDefault();
@@ -24,7 +26,7 @@ function updateLayer(e) {
 }
 
 function getLayer() {
-  return race.value.concat(gender.value,age.value);
+  return race.value.concat(gender.value, age.value, temp.value);
 }
 
 function removeLayers() {
